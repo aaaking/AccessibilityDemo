85bea0eec78c56fb85e68025a4ed127dc133f718
zhouzhihui <zhouzhihui@htgames.cn>
2018-01-23 14:44:58 +0800

    judge accessibilityservice is on or not

--- app/src/main/java/com/example/zhouzhihui/accessibilitydemo/MainActivity.kt
+++ app/src/main/java/com/example/zhouzhihui/accessibilitydemo/MainActivity.kt
@@ -1,12 +1,23 @@
 package com.example.zhouzhihui.accessibilitydemo
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.Settings
+import com.example.zhouzhihui.accessibilitydemo.access.isAccessibilityServiceOn
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        hello.also {
+            val isOn = isAccessibilityServiceOn()
+            it.text = if (isOn) "服务已经开启" else "点击开启服务"
+            it.isEnabled = !isOn
+            it.setOnClickListener {
+                startActivity(Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS))
+            }
+        }
     }
 }
--- app/src/main/java/com/example/zhouzhihui/accessibilitydemo/access/Tools.kt
+++ app/src/main/java/com/example/zhouzhihui/accessibilitydemo/access/Tools.kt
@@ -1,6 +1,9 @@
 package com.example.zhouzhihui.accessibilitydemo.access
 
 import android.accessibilityservice.AccessibilityService
+import android.content.Context
+import android.provider.Settings
+import android.text.TextUtils
 
 /**
  * Created by 周智慧 on 22/01/2018.
@@ -17,4 +20,23 @@ fun listToString(list : List<Any>?): String {
 
 fun isPrePagePacket(prePageName: String): Boolean {
     return prePageName == "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyReceiveUI" || prePageName == "com.tencent.mm.plugin.luckymoney.ui.LuckyMoneyDetailUI"
+}
+
+fun Context.isAccessibilityServiceOn(): Boolean {
+    var service = "${packageName}/${MyAccessibilityService::class.java.canonicalName}"
+    var enabled = Settings.Secure.getInt(applicationContext.contentResolver, Settings.Secure.ACCESSIBILITY_ENABLED)
+    var splitter = TextUtils.SimpleStringSplitter(':')
+    if (enabled == 1) {
+        var settingValue = Settings.Secure.getString(applicationContext.contentResolver, Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES)
+        if (settingValue != null) {
+            splitter.setString(settingValue)
+            while (splitter.hasNext()) {
+                var accessibilityService = splitter.next()
+                if (accessibilityService.equals(service, ignoreCase = true)) {
+                    return true
+                }
+            }
+        }
+    }
+    return false
 }
